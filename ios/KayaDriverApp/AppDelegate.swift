81e47cf97f28dcfe24df598112076e925c8960ff
SUBHAJIT GHOSH <subhajitghosh@SUBHAJITs-Mac-mini.local>
2025-12-01 18:05:39 +0530

    update on 01-12-25

--- ios/KayaDriverApp/AppDelegate.swift
+++ ios/KayaDriverApp/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "Group Travel",
+      withModuleName: "kayadriverapp",
       in: window,
       launchOptions: launchOptions
     )
